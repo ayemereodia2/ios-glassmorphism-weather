36ecb9d7dd2852010bb20512a68ac86a904c2740
ayemereodia2 <odiadavid2@gmail.com>
2025-05-24 19:19:31 -0300

    Initial Commit

---
+++ ModernWeatherUI/ModernWeatherUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModernWeatherUIApp.swift
+//  ModernWeatherUI
+//
+//  Created by ANDELA on 24/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ModernWeatherUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
